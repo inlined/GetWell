015c65a8728e579f8fcb308ce2e3807d31c35041
Bryan Klimt <klimt@parse.com>
2012-09-15 13:30:44 -0700

    Make the cron job send a push.

--- cloud/cron.js
+++ cloud/cron.js
@@ -13,12 +13,12 @@ var checkForDropped = function(className, timeElapsed, options) {
     success: function(results) {
       var dropped = [];
       var now = new Date();
-      Parse._.each(results, function(fridge) {
-        var updatedAt = Parse._parseDate(fridge.updatedAt);
+      Parse._.each(results, function(object) {
+        var updatedAt = Parse._parseDate(object.updatedAt);
         var elapsed = now - updatedAt;
-        // console.log(elapsed);
+        console.log(elapsed);
         if (elapsed > timeElapsed) {
-          dropped.push(fridge);
+          dropped.push(object);
         }
       });
       options.success(dropped);
@@ -31,12 +31,39 @@ var checkForDropped = function(className, timeElapsed, options) {
   });
 };
 
-checkForDropped("Fridge", 5654775, {
-  success: function(results) {
-    console.warn(results);
+var sendPush = function(object, className) {
+  console.log("Trying to send push.");
+  Parse._ajax('POST', 'https://api.parse.com/1/push', JSON.stringify({
+    _ApplicationId: "XKfhHQqQzfqP22r5gAcvZWa427AbuJpVHbFXgoOY",
+    _JavaScriptKey: "6Nk7jtlwTcXHJc07DDHht2VxPfVsr6UGF1v38axQ",
+    channels: [ "" ],
+    data: {
+      alert: "A " + className + " is stale."
+    }
+  }), function(responseText) {
+    // success
+    console.log('success');
+    console.log(responseText);
+  }, function(responseText) {
+    // error
+    console.log('error');
+    console.log(responseText);
+  });
+};
+
+var sendPushes = function(objects, className) {
+  Parse._.each(objects, function(object) {
+    sendPush(object, className);
+  });
+};
+
+checkForDropped("Fridge", 2946019, {
+  success: function(objects) {
+    console.log(objects);
+    sendPushes(objects, "Fridge");
   },
   error: function(error) {
-    console.warn(error);
+    console.error(error);
   }
 });
 
