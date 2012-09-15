7d0021de5d69f1172a1ac1d1d6df6ed60e40db38
John Roane <john.roane@gmail.com>
2012-09-15 11:32:59 -0700

    Update battery icons in map.js

--- www/js/map.js
+++ www/js/map.js
@@ -58,32 +58,32 @@ var fridges = [
     name: "Fridge on diesel",
     battery: null,
     location: {
-      latitude: -23.170033, 
-      longitude: 45.271122
+      latitude: 11.275387,
+      longitude: 30.166809
     },
     updatedAt: new Date(2012, 9, 15,  2, 31, 31),
   },{
     name: "Fridge with good battery",
     battery: 98.9,
     location: {
-      latitude: -23.170033, 
-      longitude: 45.271122
+      latitude: -25.076892,
+      longitude: 16.917366
     },
     updatedAt: new Date(2012, 9, 15,  2, 32, 30),
   },{
     name: "Fridge with low battery",
     battery: 11.2,
     location: {
-      latitude: -23.170033, 
-      longitude: 45.271122
+      latitude: -7.808963, 
+      longitude: 23.168518
     },
     updatedAt: new Date(2012, 9, 15,  2, 33, 31),
   },{
     name: "Fridge with old status",
     battery: null,
     location: {
-      latitude: -23.170033, 
-      longitude: 45.271122
+      latitude: 5.495704,
+      longitude: 39.054749
     },
     updatedAt: new Date(2012, 9, 14,  2, 33, 31),
   }
@@ -104,20 +104,37 @@ function initializeMap(domElement) {
 
 function setBatteryBars(map, fridges) {
   for (var fridgeIdx in fridges) {
-    var batteryBar = {
-      path: 'M -35,-35 L 35,-35 L 35,35 L -35,35 z',
-      fillColor: "yellow",
-      fillOpacity: 0.8,
-      scale: 1,
-      strokeColor: "gold",
-      strokeWeight: 14
-    };
     // Construct the bars for each value in fridges.
     var fridge = fridges[fridgeIdx];
+    var batteryColor = getBatteryColor(fridge.battery)
+    var batteryBar = {
+      path: 'M -5,-10 L 5,-10 L 5,10 L -5,10 z M -3,-10 L -3,-12 3,-12 3,-10 z',
+      fillColor: batteryColor,
+      fillOpacity: 0.8,
+      scale: 1,
+      strokeColor: "black",
+      strokeWeight: 4,
+      scale: 3
+    };
+
     marker = new google.maps.Marker({
       position: new google.maps.LatLng(fridge.location.latitude, fridge.location.longitude),
       icon: batteryBar,
       map: map
     });
+  }
+}
+
+function getBatteryColor(batteryLevel) {
+  if (batteryLevel == null) {
+    return "green";
+  } else if (batteryLevel < 1) {
+    return "black";
+  } else if (batteryLevel < 50) {
+    return "red";
+  } else if (batteryLevel < 75) {
+    return "orange";
+  } else {
+    return "yellow";
   }
 }
