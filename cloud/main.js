9cc9be6624f8e2f3e2514172ad91e5107d9c91d0
Thomas Bouldin <thomas@parse.com>
2012-09-15 14:04:45 -0700

    Cloud code sends pushes

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,6 @@
-function smearOnKey(foreignKey) {
+function smearOnKey(className) {
+  var Klass = Parse.Object.extend(className);
+  var foreignKey = className.toLowerCase();
   return function(request, response) {
     var smeared = request.object.get(foreignKey);
     if (!smeared) {
@@ -11,17 +13,56 @@ function smearOnKey(foreignKey) {
         smeared.set(key, value);
       }
     });
-    smeared.save(null, {
-      success:function() {
-        response.success();
-      },
-      error:function(object, error) {
-        response.error(error);
-      }
-    });
+    var saveSmeared = function() {
+      smeared.save(null, {
+        success:function() {
+          response.success();
+        },
+        error:function(object, error) {
+          response.error(error);
+        }
+      });
+    };
+
+    // feature in development:
+    if (smeared.get("usingBattery")) {
+      var query = new Parse.Query(Klass);
+      query.equalTo("objectId", smeared.id);
+      query.notEqualTo("usingBattery", true);
+      query.find({
+        success: function(results) {
+          if (!results.length) { // already off battery
+            saveSmeared();
+            return;
+          }
+          Parse.Cloud.request({
+            url: "https://api.parse.com/1/push",
+            method: "POST",
+            body: JSON.stringify({
+              _ApplicationId: "XKfhHQqQzfqP22r5gAcvZWa427AbuJpVHbFXgoOY",
+              _JavaScriptKey: "6Nk7jtlwTcXHJc07DDHht2VxPfVsr6UGF1v38axQ", 
+              channels: [results[0].id],
+              data: {
+                 alert: className + " " + results[0].get("name") + " is using battery" 
+              }
+            }),
+            headers: {
+              "Content-Type": "application/json"
+            }
+          }, function(externalResponse) {
+            // Push is a best effort; don't hold up committing data
+            saveSmeared();
+          });
+        },
+        error: function(error) {
+          response.error("Failed to look up current state: " + error.message);
+        }
+      });
+    } else {
+      saveSmeared();
+    }
   };
 }
-function maybePageUser() {
-}
-Parse.Cloud.beforeSave("FridgeStatus", smearOnKey("fridge"));
-Parse.Cloud.beforeSave("WellStatus", smearOnKey("well"));
+
+Parse.Cloud.beforeSave("FridgeStatus", smearOnKey("Fridge"));
+Parse.Cloud.beforeSave("WellStatus", smearOnKey("Well"));
